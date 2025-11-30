2be874a7814e9b73fd15f4bcd5165577bf5110a0
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-11-30 22:22:00 +0530

    creating functions

---
+++ function.js
@@ -0,0 +1,6 @@
+function greeting() {
+    console.log("Hello, welcome to the program!");
+}
+console.log("Starting the program...");
+setTimeout(greeting, 2000);          // Call greeting after 2 seconds 
+console.log("Program is running...");
